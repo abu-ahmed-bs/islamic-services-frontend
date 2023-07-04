5825a9f2ceb7008ecec04a48c33eb32b808b5cbc
Baraa Seedeena <qazt66@gmail.com>
2023-07-04 17:33:43 +0300

    enhanced slider

--- components/client/dekrCard.tsx
+++ components/client/dekrCard.tsx
@@ -8,27 +8,32 @@ import {
   CardTitle,
 } from "@/components/ui/card"
 import CountButton from "./countButton";
+import { Badge } from "../ui/badge";
+import { Separator } from "@radix-ui/react-separator";
 
 
 type DekrCardProps = {
   item: any,
+  itemCount: number,
+  font:any
 }
 
 
 
-export default function DekrCard({item}: DekrCardProps) {
+export default function DekrCard({item,itemCount,font}: DekrCardProps) {
 
   
   return(
-    <Card className='m-4 bg-[#2D4263] text-[#ECDBBA]'>
+    <Card className={`relative flex flex-col w-screen h-screen m-4 mx-auto rounded-none bg-slate-100 bg-opacity-20 md:bg-white md:w-2/3 md:h-80 md:rounded-md`}>
     <CardHeader>
-      <CardTitle className='text-[#FFD966]'>{item.category}</CardTitle>
-      {item.description && <CardDescription className='text-[#FFD966]'>{item.description}</CardDescription>}
+      <CardTitle className="flex flex-row gap-2"><Badge className="self-center">{itemCount}</Badge>{item.category}</CardTitle>
+      {item.description && <CardDescription>{item.description}</CardDescription>}
     </CardHeader>
-    <CardContent>
-      <p>{item.content}</p>
+    <CardContent className={`h-full ${font.className}`}>
+      <p className='text-xl'>{item.content}</p>
     </CardContent>
-    <CardFooter>
+    <hr />
+    <CardFooter className='inset-x-0 bottom-0 m-3'>
       <CountButton totalCount={item.count} />
     </CardFooter>
   </Card>    
